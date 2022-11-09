fe9cfd5053ae820b8fdabc9b54188d1edc45d3e0
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-11-09 19:54:21 +0100

    versione senza bonus fatta

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-// Consigli del giorno:  :party_wizard:
+// Consigli del giorno: 
 // Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 // Ad esempio:
 // Di cosa ho bisogno per generare i numeri?
@@ -18,25 +18,50 @@
 //realizzo la var. del bottone attivarla tramite event listner
 const btnGenera = document.getElementById("btn-play");
 //creeo anche la costante di grid container da usare per dopo
-const gridContainer = document.querySelector("grid-container");
+const gridContainer = document.querySelector(".grid-container");
 const numCelle = 10;
 
+
+
 //ora attivo il bottone play 
-btnGenera.addEventListener("click", function () {
-    console.log(this);
 
 
+// btnGenera.addEventListener("click", function () {
+//     console.log(this);
+    
+    
+// });
 
-});
 
-function btnGenera(){
-
+function generaGriglia(numCelle){
+    
     const totalCelle = parseInt(numCelle * numCelle);
-
+    
     for (let i = 0; i < totalCelle; i++){
-        gridContainer.innerHTML += `<div class='grid-cell' style='width: calc( 100% / ${ numCelle })'></div>`;
+        
+        
+        //creo una cella nuova per renderla cliccabile
+        const newCella = document.createElement("div");
+        
+        //assegno gli stili alla new cella
+        newCella.classList.add("grid-cell");
+        newCella.style.width = `calc(100% / ${numCelle})`;
+        
+        newCella.innerHTML = i + 1;
+        
+        newCella.addEventListener("click", function (){
+            this.classList.toggle("bg-primary");
+            console.log(this.textContent);
+        });
+        gridContainer.append( newCella );
     }
 }
 
-btnGenera(totalCelle);
-console.log(btnGenera(totalCelle));
+
+btnGenera.addEventListener("click", function () {
+    console.log(this);
+    
+    generaGriglia(numCelle);
+    
+});
+
